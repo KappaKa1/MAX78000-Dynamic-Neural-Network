f458573377ed1c5c36026b878b68a257972d041f
KappaKa1 <147872475+KappaKa1@users.noreply.github.com>
2024-08-23 14:38:04 +0100

    Update UART_KC.c

--- final-proj/UART_KC.c
+++ final-proj/UART_KC.c
@@ -26,27 +26,28 @@ void UART_Handler(void)
 
 void init_UART(void)
 {
-	//J1_1 is receiving data, J1_0 is transmitting data
+	// Initiates the UART for Communication
 	error = 0;
-    NVIC_ClearPendingIRQ(UART_SETTING);
-    NVIC_DisableIRQ(UART_SETTING);
-    NVIC_SetVector(UART_SETTING, UART_Handler);
-    NVIC_EnableIRQ(UART_SETTING);
+    	NVIC_ClearPendingIRQ(UART_SETTING);
+    	NVIC_DisableIRQ(UART_SETTING);
+    	NVIC_SetVector(UART_SETTING, UART_Handler);
+    	NVIC_EnableIRQ(UART_SETTING);
 
 	error = MXC_UART_Init(UART_NO, UART_BAUD, MXC_UART_APB_CLK);
-    if (error < E_NO_ERROR) {
-        printf("-->Error initializing UART: %d\n", error);
-        printf("-->Example Failed\n");
+    	if (error < E_NO_ERROR) {
+        	printf("-->Error initializing UART: %d\n", error);
+        	printf("-->Example Failed\n");
 
-        while (1) {}
-    }
-    else {
-        printf("-->UART Initialized\n\n");
-    }
+        	while (1) {}
+    	}
+    	else {
+        	printf("-->UART Initialized\n\n");
+    	}
 }
 
 void UART_setup(uint8_t* buffer, uint8_t* output)
 {
+	// Sets up the Buffer's that will be used for Communication
 	read_req.uart = UART_NO;
 	read_req.rxData = buffer;
 	read_req.txData = output;
@@ -58,6 +59,7 @@ void UART_setup(uint8_t* buffer, uint8_t* output)
 
 void UART_start(void)
 {
+	// Starts the Communication protocol between 2 devices
 	MXC_UART_ClearRXFIFO(UART_NO);
 	error = MXC_UART_Transaction(&read_req);
 }
